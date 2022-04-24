079f01d36e61fd0b7e6bf90cffa80aa5f45192ca
dirtyjesuss <thepw@bk.ru>
2022-04-24 20:44:53 +0300

    update model structure

--- unTILED/unTILED/Domain/Models/Elements/TextElement.swift
+++ unTILED/unTILED/Domain/Models/Elements/TextElement.swift
@@ -6,9 +6,18 @@
 //
 
 struct TextElement: CellElement {
+    
+    // MARK: - Nested types
+
+    enum Alignment {
+        case left, right, center, justify
+    }
+
+    // MARK: - Fields
+    
     var content: String
     var textColor: RGBA
-    var alignment: TextAlignment
+    var alignment: Alignment
     var font: String
     var fontSize: Int
 }
